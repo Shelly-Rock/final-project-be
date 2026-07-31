869f3ff33bb54605d3c8a7fd42e33deb86f96209
DuongQuang132 <trinhduongquang132@gmail.com>
2026-07-31 11:48:07 +0700

    feature/update-pagination.util-and-date.util

---
+++ src/core/utils/index.ts
@@ -0,0 +1,4 @@
+export * from './codeGenerator.util';
+export * from './crypto.util';
+export * from './date.util';
+export * from './pagination.util';
--- src/modules/student/dto/request/index.ts
+++ src/modules/student/dto/request/index.ts
@@ -1,3 +1,3 @@
-export * from "./createStudentReq.dto";
+export * from "./createStudentREQ.dto";
 export * from "./removeStudentREQ.dto";
 export * from "./updateStudentREQ.dto";
