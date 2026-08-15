557f2cf63aa535c51ecb4140b5bbd5f2c3d0aa8f
Tuyền Nguyễn <thuylucbonjajz@gmail.com>
2026-08-15 17:19:59 +0700

    fix: lose prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,4 @@ export default defineConfig({
     path: 'prisma/migrations',
     seed: 'ts-node -r tsconfig-paths/register prisma/seed.ts',
   },
-  datasource: {
-    url: 'postgresql://postgres:1@localhost:5432/be_db?schema=public&connection_limit=1',
-  },
 });
